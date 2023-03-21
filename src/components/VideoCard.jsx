1160d8def1552bb7c3b030b08d6f208d374cf8d5
unknown <prabhunath001600@gmail.com>
2023-03-21 13:07:58 +0530

    ytclone

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,7 +4,7 @@ import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
 import {
-  demoThumbnailUrl,
+  
   demoVideoUrl,
   demoVideoTitle,
   demoChannelTitle,
